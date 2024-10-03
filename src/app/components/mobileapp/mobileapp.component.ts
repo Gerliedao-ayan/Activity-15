e5acc421207c696b6017cf958f151e0cc4d594e7
Gerliedao-ayan <gerliedaoayan02@gmail.com>
2024-10-03 23:36:55 +0400

    mobileapp

--- src/app/components/mobileapp/mobileapp.component.ts
+++ src/app/components/mobileapp/mobileapp.component.ts
@@ -8,20 +8,19 @@ interface App {
   styleUrl: './mobileapp.component.css'
 })
 export class MobileappComponent {
-  appList: App[] = [
+  appLis: App[] = [
     { name: "Instagram" },
     { name: "WhatsApp" },
-    { name: "YouTube" }
   ];
   newApp: string = '';
 
   addApp() {
     const newApp: App = { name: this.newApp };
-    this.appList.push(newApp);
+    this.appLis.push(newApp);
     this.newApp = '';
   }
 
   deleteApp(index: number) {
-    this.appList.splice(index, 1);
+    this.appLis.splice(index, 1);
   }
 }
