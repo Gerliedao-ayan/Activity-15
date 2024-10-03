7bcd531225bda9d767197e3d3ebcbb4d4615a276
Gerliedao-ayan <gerliedaoayan02@gmail.com>
2024-10-03 23:00:17 +0400

    phone

--- src/app/components/phone/phone.component.ts
+++ src/app/components/phone/phone.component.ts
@@ -12,21 +12,21 @@ interface Contact {
   styleUrl: './phone.component.css'
 })
 export class PhoneComponent {
-  contactList: Contact[] = [
-    { name: "Jerome Beriso", phoneNumber: "123-456-7890" },
-    { name: "Je rome", phoneNumber: "987-654-3210" }
+  contactLis: Contact[] = [
+    { name: "Gerlie Dao-ayan", phoneNumber: "09518942333" },
+    { name: "garl", phoneNumber: "09380798236" }
   ];
   name: string = '';
   phoneNumber: string = '';
 
   addContact() {
     const newContact: Contact = { name: this.name, phoneNumber: this.phoneNumber };
-    this.contactList.push(newContact);
+    this.contactLis.push(newContact);
     this.name = '';
     this.phoneNumber = '';
   }
 
   deleteContact(index: number) {
-    this.contactList.splice(index, 1);
+    this.contactLis.splice(index, 1);
   }
 }
