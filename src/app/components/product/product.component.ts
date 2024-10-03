cf3b0833ce60726c1e5132ee5c08d536f8011f53
Gerliedao-ayan <gerliedaoayan02@gmail.com>
2024-10-03 22:29:19 +0400

    produclist

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  productList: string[] = ["Laptop", "Smartphone", "Headphones"];
+  productLis: string[] = ["phone", "tablet", "headset"];
   item: string = '';
 
   addItems() {
-    this.productList.push(this.item);
+    this.productLis.push(this.item);
   }
 
   deleteProduct(index: number) {
-    this.productList.splice(index, 1);
+    this.productLis.splice(index, 1);
   }
 }
